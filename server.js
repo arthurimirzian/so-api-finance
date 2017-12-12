4decb68b046c66b297e17059dc4ad65741dac8f2
Arthur Imirzian <arthur.imirzian@gmail.com>
2017-12-12 17:26:46 +0100

    INIT

---
+++ server.js
@@ -0,0 +1,23 @@
+var express     = require('express')
+  , app 	    = express()
+;
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.get('/compteFinancier', function(req, res) {
+  res.set('Content-Type', 'text/xml');
+  return res.render('compteFinancier');
+});
+app.get('/estFacture', function(req, res) {
+  res.set('Content-Type', 'text/xml');
+  var a = Date.now();
+  var is = isOdd(a);
+  return res.render('estFacture',{
+    is: is
+  });
+});
+function isOdd(num) { return num % 2;}
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
