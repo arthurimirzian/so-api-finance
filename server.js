befb98f3ef0634e25e353833f6b20edd455f47ce
Arthur Imirzian <arthur.imirzian@gmail.com>
2017-12-13 14:44:42 +0100

    update data

--- server.js
+++ server.js
@@ -6,20 +6,25 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.get('/compteFinancier', function(req, res) {
+  const epj = req.params.EPJ;
+  if(epj==null||epj==''){
+    return res.render('error');
+  }
   res.set('Content-Type', 'text/xml');
   return res.render('compteFinancier',{
-    epj: req.params
+    epj: epj
   });
 });
 app.get('/estFacture', function(req, res) {
+  const epj = req.params.EPJ;
+  if(epj==null||epj==''){
+    return res.render('error');
+  }
   res.set('Content-Type', 'text/xml');
-  var a = Date.now();
-  var is = isOdd(a);
   return res.render('estFacture',{
-    is: is
+    is: (parseInt(epj) % 2)
   });
 });
-function isOdd(num) { return num % 2;}
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
